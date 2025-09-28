497f44cb46fb0fb999f15ced22b61e169f7ca256
MD. ROQNUZZAMAN ROZEN <rozensoft007@gmail.com>
2025-09-28 11:42:10 +0600

    Update CategoriesTable.jsx

--- src/components/admin/CategoriesTable.jsx
+++ src/components/admin/CategoriesTable.jsx
@@ -27,7 +27,7 @@ function CategoriesTable() {
   const addCategory = async () => {
     try {
       await axios.post(
-        "${import.meta.env.VITE_API_URL}/categories",
+        `${import.meta.env.VITE_API_URL}/categories`,
         { name: newCategory },
         { headers: { Authorization: `Bearer ${token}` } }
       );
